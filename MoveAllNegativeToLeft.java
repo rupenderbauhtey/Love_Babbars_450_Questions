99aad200577d6a43892688cf222b06b8dd44d594
Rupender Singh <46915626+rupenderbauhtey@users.noreply.github.com>
2021-10-11 02:32:21 +0530

    Create MoveAllNegativeToLeft.java

---
+++ MoveAllNegativeToLeft.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MoveAllNegatives {
+
+	public static void main(String[] args) {
+		int n; 
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Enter the size of the array you want to create");
+		n=sc.nextInt(); 
+		int[] arr= new int[n]; 
+		for(int i=0; i<arr.length; i++)
+		{
+			arr[i]= sc.nextInt(); 
+		}
+		int i=0; 
+		int j=0; 
+		while ( i<arr.length)
+		{
+			if( arr[i]<0)
+			{
+				int temp=arr[i]; 
+				arr[i]=arr[j]; 
+				arr[j]=temp; 
+				j++;
+			}
+			i++;
+		}
+		System.out.println("Code Running");
+		for(int k=0; k<arr.length;k++)
+		{
+			System.out.println(arr[k]);
+		}
+	}
+}
